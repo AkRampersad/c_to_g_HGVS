ab51c882c65bc98bdc5a81dd043d2399aa889775
Akash <akashjrampersad@gmail.com>
2024-01-10 16:57:58 -0500

    refactored slightly to separate conversion function from file. fixed insertions that can be fixed

--- c_to_g.py
+++ c_to_g.py
@@ -9,32 +9,35 @@ hp = hgvs.parser.Parser()
 vm = hgvs.assemblymapper.AssemblyMapper(
     hdp, assembly_name='GRCh38')
 
-file = open('hgvs_c_to_g_variant_list.txt', 'r')
+def chrom_to_genome(variant_file): 
+    file = open(variant_file, 'r')
 
-var_dict = {}
+    var_dict = {}
 
-for line in file:
-    line_dict = {}
-    proc_line = line.strip()
-    if proc_line.startswith('N'):
-        
-        try:
-            var = hp.parse_hgvs_variant(proc_line)
-            vm.c_to_g(var)
-        except Exception as e: 
-            var_dict[proc_line] = f'invalid format error {e}'
-        else:
-            var = hp.parse_hgvs_variant(proc_line)
-            var_g = vm.c_to_g(var)
-            line_dict['start'] = str(var_g.posedit.pos.start)
-            line_dict['end'] = str(var_g.posedit.pos.end)
-            line_dict['ref'] = str(var_g.posedit.edit.ref)
-            if str(var_g).find('dup') == -1 and str(var_g).find('inv') == -1:
-                line_dict['alt'] = var_g.posedit.edit.alt
+    for line in file:
+        line_dict = {}
+        proc_line = line.strip()
+        if proc_line.startswith('N'):
 
-            var_dict[proc_line] = line_dict
+            try:
+                var = hp.parse_hgvs_variant(proc_line)
+                vm.c_to_g(var)
+            except Exception as e: 
+                var_dict[proc_line] = f'invalid format error {e}'
+            else:
+                var = hp.parse_hgvs_variant(proc_line)
+                var_g = vm.c_to_g(var)
+                line_dict['start'] = str(var_g.posedit.pos.start)
+                line_dict['end'] = str(var_g.posedit.pos.end)
+                line_dict['ref'] = str(var_g.posedit.edit.ref)
+                if str(var_g).find('dup') == -1 and str(var_g).find('inv') == -1:
+                    line_dict['alt'] = var_g.posedit.edit.alt
 
-with open('results.json', 'w') as outfile:
-    json.dump(var_dict, outfile, indent = 2)
+                var_dict[proc_line] = line_dict
+
+    with open('results.json', 'w') as outfile:
+        json.dump(var_dict, outfile, indent = 2)
+
+chrom_to_genome('hgvs_c_to_g_variant_list.txt')
 
 
